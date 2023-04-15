badf6edd511b79bb54e92d3533f7db75fcfddaa0
63070014 <63070014@kmitl.ac.th>
2023-04-15 19:05:35 +0700

    addJob page :package:

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Prelogin from './screen/Prelogin'
 import Login from './screen/login'
 import Signup from './screen/signup'
 import Booking from './screen/Booking'
+import AddJob from './screen/AddJob'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/login' element = {<Login/>}/>
         <Route path='/signup' element = {<Signup/>}/>
         <Route path='/booking' element = {<Booking/>}/>
+        <Route path='/addJob' element = {<AddJob/>}/>
       </Routes>
     </BrowserRouter>
   )
--- src/screen/AddJob.jsx
+++ src/screen/AddJob.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import logo from "../assets/image/logo1.png";
 import profile from "../assets/image/profile.svg";
-import note from "../assets/image/note_icon.svg";
+import notification from "../assets/image/notification_icon.svg";
 import passenger from "../assets/image/passenger_icon.svg";
-import sex from "../assets/image/sex_icon.svg";
+import time from "../assets/image/time_icon.svg";
 
 import "../css/map.css";
 import { Link } from "react-router-dom";
-export default function Booking() {
+export default function AddJob() {
   return (
     <div className="flex flex-col bg-map h-screen">
       <div className="flex justify-between p-6 space-x-3 bg-white">
         <div className="w-24">
           <img src={logo} alt="" />
         </div>
-        <div className="w-44 h-6 border-b-2 border-[#3D5EA3]"></div>
+        <div className="w-32 h-6 border-b-2 border-[#3D5EA3]"></div>
+        <div className="w-22 flex justify-center items-center">
+          <img src={notification} alt="" />
+        </div>
         <div className="w-22 flex justify-center items-center">
           <img src={profile} alt="" />
         </div>
@@ -52,10 +55,10 @@ export default function Booking() {
             <div className="h-12 border-solid border-4 border-l border-[#848181]"></div>
             <div className="relative w-full flex items-center jura">
               <div className="w-8 absolute text-gray-400 left-3 text-xl">
-                <img src={note} alt="" />
+                <img src={time} alt="" />
               </div>
               <div className="h-12 p-3 pl-9 w-full bg-white border-solid border-2 border-[D2D4D6] outline-none">
-                Notes to driver
+                Pick-up time
               </div>
             </div>
           </div>
@@ -75,21 +78,10 @@ export default function Booking() {
               </div>
             </div>
           </div>
-          <div className="flex w-5/6">
-            <div className="h-12 border-solid border-4 border-l border-[#848181]"></div>
-            <div className="relative w-full flex items-center jura">
-              <div className="w-8 absolute text-gray-400 left-2.5 text-xl">
-                <img src={sex} alt="" />
-              </div>
-              <div className="p-3 h-12 pl-9 flex w-full justify-between bg-white border-solid border-2 border-[D2D4D6]">
-                <div className=" outline-none">Same Gender driver</div>
-                <input type="checkbox" name="" id="" />
-              </div>
-            </div>
-          </div>
+
           <Link to="" className="w-5/6 text-center mt-2 lexend mb-10">
             <button className="bg-[#3D5EA3] w-full p-2 text-white font-normal ">
-              BOOK
+              ADD JOB
             </button>
           </Link>
         </div>
