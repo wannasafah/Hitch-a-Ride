6974460b6c60087e94fa110d13d06d7666b50a3b
fahfiily <63070156@kmitl.ac.th>
2023-04-15 14:53:25 +0700

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Splash from './screen/Splash'
 import Prelogin from './screen/Prelogin'
+import Login from './screen/login'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element = {<Splash/>}/>
         <Route path='/prelogin' element = {<Prelogin/>}/>
+        <Route path='/login' element = {<Login/>}/>
       </Routes>
     </BrowserRouter>
   )
